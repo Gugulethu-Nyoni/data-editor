199a7dd9101fcb6a0388150da0700ac6ddb788f8
Gugu <gugunnn@gmail.com>
2026-08-26 14:58:08 +0200

    feat: expose data editor public API

---
+++ index.js
@@ -0,0 +1,27 @@
+export { default as createDefaultRegistry } from './core/createDefaultRegistry.js';
+export { default as DataEditor } from './core/DataEditor.js';
+export { default as EditorRegistry } from './core/EditorRegistry.js';
+export { default as MetadataResolver } from './core/MetadataResolver.js';
+export { default as MutationManager } from './core/MutationManager.js';
+
+export { default as smQLAdapter } from './api/smQLAdapter.js';
+
+export { default as BaseEditor } from './editors/BaseEditor.js';
+
+export { default as StringEditor } from './editors/scalar/StringEditor.js';
+export { default as IntEditor } from './editors/scalar/IntEditor.js';
+export { default as NumberEditor } from './editors/scalar/NumberEditor.js';
+export { default as BigIntEditor } from './editors/scalar/BigIntEditor.js';
+export { default as FloatEditor } from './editors/scalar/FloatEditor.js';
+export { default as DecimalEditor } from './editors/scalar/DecimalEditor.js';
+export { default as BooleanEditor } from './editors/scalar/BooleanEditor.js';
+export { default as DateTimeEditor } from './editors/scalar/DateTimeEditor.js';
+export { default as JsonEditor } from './editors/scalar/JsonEditor.js';
+export { default as BytesEditor } from './editors/scalar/BytesEditor.js';
+export { default as EnumEditor } from './editors/scalar/EnumEditor.js';
+
+export { default as CommaSeparatedEditor } from './editors/structured/CommaSeparatedEditor.js';
+export { default as PredefinedKeyValueEditor } from './editors/structured/PredefinedKeyValueEditor.js';
+export { default as CustomKeyValueEditor } from './editors/structured/CustomKeyValueEditor.js';
+
+export * from './metadata/contract.js';
